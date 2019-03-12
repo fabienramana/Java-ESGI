e0385681578d80337dd4c27ebacb8c6c3b77a35b
fabienramana <fabien.rmnd@gmail.com>
2019-03-12 13:26:46 +0100

    display-1

--- main.java
+++ main.java
@@ -1,7 +1,20 @@
 class main{
 
     public static void main(String args[]){
-        System.out.println("0");
+        if(args.length == 1){
+            System.out.println(args[0]);
+            if(args[0] == "1"){
+            //    if(args[0].equals("1")){
+                    System.out.println("0");
+              //  }
+            }
+        }
+        else if(args.length == 0){
+            System.out.println("Pas d'arguments !");
+        }
+        else{
+            System.out.println("Trop d'arguments !");
+        }
     }
 }
  
