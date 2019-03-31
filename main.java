a1e275162d79c212d9b6ae68ca514cc998b391f3
fabien <f.ramanadapoulle@wesharebonds.com>
2019-03-31 21:08:17 +0200

    -display-1-final

--- main.java
+++ main.java
@@ -1,12 +1,10 @@
 class main{
 
     public static void main(String args[]){
+        
         if(args.length == 1){
-            System.out.println(args[0]);
-            if(args[0] == "1"){
-            //    if(args[0].equals("1")){
-                    System.out.println("0");
-              //  }
+            if(args[0].equals("1")){
+                System.out.println(args[0]);
             }
         }
         else if(args.length == 0){
