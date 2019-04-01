b641630a09f06d1ec81c5cca55d517b44e379670
fabien <f.ramanadapoulle@wesharebonds.com>
2019-04-01 14:49:18 +0200

    display-number-from-binary-form

--- main.java
+++ main.java
@@ -6,24 +6,12 @@ class main{
             int number = 0;
             
             try{
-                number = Integer.parseInt(args[0]);
+                number = Integer.parseInt(args[0], 2);
             }
             catch(NumberFormatException e){
                 System.out.println("L'argument donné n'est pas une nombre !");
             }
-            if(number >= 2){
-                int half = 0;
-                half = number/2;
-                number = number%2;
-                half = half%2;
-                System.out.print(half);
-                System.out.print(number);
-                System.out.print("\n");
-            }
-            else{
-                number = number%2;
-                System.out.println(number);
-            }
+            System.out.println(number);
         }
         else if(args.length == 0){
             System.out.println("Pas d'arguments !");
