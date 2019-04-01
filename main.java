b4bd58f63f9f5d8863bae7c54dd72e73804998c8
fabien <f.ramanadapoulle@wesharebonds.com>
2019-04-01 11:26:53 +0200

    step-1.4

--- main.java
+++ main.java
@@ -4,13 +4,25 @@ class main{
         
         if(args.length == 1){
             int number = 0;
+            
             try{
                 number = Integer.parseInt(args[0]);
-                number = number%2;
-                System.out.println(number);
             }
             catch(NumberFormatException e){
                 System.out.println("L'argument donné n'est pas une nombre !");
+            }
+            if(number >= 2){
+                int half = 0;
+                half = number/2;
+                number = number%2;
+                half = half%2;
+                System.out.print(half);
+                System.out.print(number);
+                System.out.print("\n");
+            }
+            else{
+                number = number%2;
+                System.out.println(number);
             }
         }
         else if(args.length == 0){
